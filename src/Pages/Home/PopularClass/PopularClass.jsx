5b1cb1e9225d69f8274eed14b261ac2fbbdb1ebe
mdyasin111 <mdyasin7636@gmail.com>
2023-09-07 23:01:53 +0600

    updated

--- src/Pages/Home/PopularClass/PopularClass.jsx
+++ src/Pages/Home/PopularClass/PopularClass.jsx
@@ -13,10 +13,10 @@ const PopularClass = () => {
 
   return (
     <div>
-      <h2 className="text-center font-bold text-3xl my-16">Popular Classes </h2>
-      <div className="grid md:grid-cols-3 gap-10 md:ml-8 sm:ml-12">
+      <h2 className="text-center font-bold text-3xl mt-10">Popular Classes</h2>
+      <div className="grid md:grid-cols-3">
         {classes.slice(0, 6).map((classItem) => (
-          <div key={classItem._id} className="card w-96 bg-base-100 shadow-lg shadow-black hover:scale-95 duration-500">
+          <div key={classItem._id} className="card md:w-96 w-80 bg-base-100 shadow-lg shadow-black hover:scale-95 duration-500 mx-auto my-5 mt-10">
             <figure className="px-6 py-6">
               <img src={classItem.classImage} className="rounded-xl shadow-md shadow-black" alt="" />
             </figure>
--- src/Pages/Home/PopularInstructor/PopularInstructor.jsx
+++ src/Pages/Home/PopularInstructor/PopularInstructor.jsx
@@ -16,16 +16,16 @@ const PopularInstructor = () => {
 
   return (
     <div>
-      <h2 className="text-center font-bold text-3xl my-16">
+      <h2 className="text-center font-bold text-3xl mt-10">
         Popular Instructors
       </h2>
-      <div className="grid md:grid-cols-3 gap-7 md:ml-8 sm:ml-12">
+      <div className="grid md:grid-cols-3">
         {instructors.map((instructor) => (
-          <div key={instructor._id} className="card w-96 bg-base-100 shadow-lg shadow-black hover:scale-105 duration-500 mt-4">
+          <div key={instructor._id} className="card md:w-96 w-80 bg-base-100 shadow-lg shadow-black hover:scale-105 duration-500 mx-auto mt-10 my-5">
             <figure>
               <img src={instructor.photo} />
             </figure>
-            <div className="card-body items-center text-center">
+            <div className="card-body items-center text-center pt-5 pb-5">
               <h2 className="card-title">{instructor.name}</h2>
             </div>
           </div>
